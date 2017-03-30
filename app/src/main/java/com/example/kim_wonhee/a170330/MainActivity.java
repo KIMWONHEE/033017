259d42ba0d5176b99105d54bbeb05f713b875980
김원희 <kim_wonhee@gim-wonhuiui-MacBook-Air.local>
2017-03-30 16:46:18 +0900

    2017년 3월 30일
    실습 05.

---
+++ app/src/main/java/com/example/kim_wonhee/a170330/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kim_wonhee.a170330;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("HYPasta 예약시스템");
+    }
+
+    public void onMyClick(View v) {
+
+        if (v.getId() == R.id.button) {
+
+        }
+
+
+    }
+}
